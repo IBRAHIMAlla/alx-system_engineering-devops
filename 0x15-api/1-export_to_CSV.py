b18f8280a5d8abdffc9fa75822aca437776ef04d
IBRAHIMAlla <brahimalla973@gmail.com>
2024-01-28 23:10:42 -0800

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,19 +4,34 @@ Export data in the CSV format.
 '''
 
 import csv
+import json
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    ui = argv[1]
-    u = "https://jsonplaceholder.typicode.com/users/{}".format(ui)
-    us = requests.get(url, verify=False).json()
-    u = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        ui)
-    td = requests.get(u, verify=False).json()
-    with open("{}.csv".format(ui), 'w', newline='') as csvfile:
-        tsk = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for t in td:
-            tsk.writerow([int(ui), us.get('username'),
-                                 t.get('completed'),
-                                 t.get('title')])
+
+if __name__ == "__main__":
+
+    se = requests.Session()
+
+    ie = argv[1]
+    u = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(ie)
+    nu = 'https://jsonplaceholder.typicode.com/users/{}'.format(ie)
+
+    em = se.get(u)
+    emName = se.get(nu)
+
+    jr = em.json()
+    us = emName.json()['username']
+
+    tl = 0
+
+    for d in jr:
+        if d['completed']:
+            tl += 1
+
+    fCSV = ie + '.csv'
+
+    with open(fCSV, "w", newline='') as csvf:
+        wt = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_ALL)
+        for m in jr:
+            wt.writerow([ie, us, m.get('completed'), m.get('title')])
